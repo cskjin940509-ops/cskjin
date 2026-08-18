d2335e76033c8c02863a04e2c8186e5dbba063bb
cskjin940509-ops <cskjin940509@gmail.com>
2026-08-18 17:29:18 +0800

    Bump Android app to v0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.rui.astockstrategy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     buildFeatures { compose = true }
